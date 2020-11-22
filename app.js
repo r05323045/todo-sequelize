3cf5d4c9779e01cbbeb643d56f589e86dd2646ed
r05323045 <r05323045@ntu.edu.tw>
2020-11-22 22:37:52 +0800

    add logout feature

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 
-const usePassport = require('./config/passport')
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 
 const app = express()
 const PORT = 3000
@@ -13,16 +13,17 @@ const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
-app.use(routes)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 usePassport(app)
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
